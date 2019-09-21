599d60c6265032e513f5349029e0eababc4e37b0
Brandon <614018606@qq.com>
2019-09-21 23:42:21 +0800

    2019/9/21

--- 0-100/62.unique-paths.java
+++ 0-100/62.unique-paths.java
@@ -5,7 +5,17 @@
  */
 class Solution {
     public int uniquePaths(int m, int n) {
-        
+        int[][] result = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if(j == 0 || i == 0){
+                    result[i][j] = 1;
+                }else{
+                    result[i][j] = result[i-1][j] + result[i][j-1];
+                }
+            }
+        }
+        return result[m-1][n-1];
     }
 }
 
