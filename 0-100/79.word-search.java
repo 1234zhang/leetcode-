31cedbe8b5bb11c242c8cdfe82089acba3639d19
Brandon <614018606@qq.com>
2019-10-01 00:02:14 +0800

    2019\9\30

--- 0-100/79.word-search.java
+++ 0-100/79.word-search.java
@@ -36,8 +36,58 @@
  * 
  */
 class Solution {
+    char[][] board;
+    // 表示有多少行
+    int m;
+    // 表示有多少列
+    int n;
+    boolean[][] marked;
+    String word;
+    int[][] direction = {
+        {-1,0}, {0,1},{1,0},{0,-1}
+    };
     public boolean exist(char[][] board, String word) {
+        this.board = board;
+        this.word = word;
         
+        m = board.length;
+        if(m < 1){
+            return false;
+        }
+        n = board[0].length;
+        marked = new boolean[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if(dfs(i, j, 0)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean dfs(int i, int j, int start){
+        if(start == word.length() - 1 && board[i][j] == word.charAt(word.length() - 1)){
+            return true;
+        }
+        if(board[i][j] == word.charAt(start)){
+            for (int h = 0; h < direction.length; h++) {
+                marked[i][j] = true;
+                int newI = i + direction[h][0];
+                int newJ = j + direction[h][1];
+                if(inArea(newI, newJ) && !marked[newI][newJ]){
+                    if(dfs(newI, newJ, start + 1)){
+                        return true;
+                    }
+                }
+            }
+            marked[i][j] = false;
+        }
+        return false;
+    }
+
+    private boolean inArea(int i, int j){
+        return i >= 0 && j >= 0 && i < board.length && j < board[0].length;
     }
 }
 
